035b909e3936b72838b2a6f0ad57596d25e39e22
(Jesse) Hassler Thurston <jthurst3@u.rochester.edu>
2014-12-02 15:25:23 -0500

    add command-line arguments

--- dry/DRY.java
+++ dry/DRY.java
@@ -22,8 +22,10 @@ import japa.parser.ASTHelper;
 import japa.parser.ast.body.Parameter;
 import japa.parser.ast.visitor.GenericVisitor;
 import japa.parser.ast.visitor.VoidVisitorAdapter;
+
 import java.io.FileInputStream;
 import java.util.List;
+import java.util.Arrays;
 
 // import dry.visitors.*;
 
@@ -33,41 +35,67 @@ import java.util.List;
 public class DRY {
 
     static boolean comments = false;
-	public static void main(String[] args) throws Exception {
+    static String filename;
+    static String heuristic;
+    
+    static String[] files = new String[] {"HelloWorld", "HelloWorldWet", "DoubleFor", "DoubleForWet", "Factorial", "FactorialWet", "DivideByZero"};
+    static String[] heuristics = new String[] {"ZeroVisitor", "Iteration1Visitor"};
+    
+    public static void main(String[] args) throws Exception {
         // check for print statements
         if(args.length >= 1 && args[0].equals("-c")) {
             comments = true;
         }
+        // parse command line arguments
+        parseCommandArgs(args);
 
         // creates an input stream for the file to be parsed
         String prefix = "samples/";
         String extension = ".java";
-        String[] files = new String[] {"HelloWorld", "HelloWorldWet", "DoubleFor", "DoubleForWet", "Factorial", "FactorialWet", "DivideByZero"};
-        String[] heuristics = new String[] {"ZeroVisitor", "Iteration1Visitor"};
 
-        for (int h = 0; h < heuristics.length; h++) {
-            for (int f = 0; f < files.length; f++) {
-                // test the heuristic on the file
-                Double dryScore = test(heuristics[h], prefix + files[f] + extension);
-                System.out.println("DRY Score for " + files[f] + " using heuristic " + heuristics[h] + " is: " + dryScore);
+        // test the heuristic on the file
+        Double dryScore = test(heuristic, filename);
+        System.out.println("DRY Score for " + filename + " using heuristic " + heuristic + " is: " + dryScore);
 
-                /*FileInputStream in = new FileInputStream(prefix + files[f] + extension);
 
-                CompilationUnit cu;
-                try {
-                    // parse the file
-                    cu = JavaParser.parse(in);
-                } finally {
-                    in.close();
-                }
 
-                // run the DRY heuristic
-                Double dryScore = new ZeroVisitor(comments).visit(cu, null);
-                System.out.println("DRY Score for " + files[f] + " is: " + dryScore);*/
-                
-            }
-        }
     };
+
+    /**
+     * Parses command line arguments
+     * Arguments must be of this form: <heuristic> <filename> [-c]
+     * @author J. Hassler Thurston
+     * CSC200H Research Project
+     * Fall 2014
+    */
+    public static void parseCommandArgs(String[] args) {
+        if(args.length < 2) {
+            printUsage();
+            System.exit(0);
+        }
+        // check if we have a valid heuristic
+        heuristic = args[0];
+        if(!Arrays.asList(heuristics).contains(heuristic)) {
+            System.err.println("invalid heuristic... " + heuristic);
+            System.exit(1);
+        }
+        // check if we have a valid filename
+        filename = args[1];
+        if(args.length >= 3 && args[2].equals("-c")) {
+            comments = true;
+        }
+    }
+    /**
+     * Prints usage information
+     * @author J. Hassler Thurston
+     * CSC200H Research Project
+     * Fall 2014
+    */
+    public static void printUsage() {
+        String usage = "";
+        usage += "DRY usage: java DRY <heuristic> <filename> [-c]";
+        System.out.println(usage);
+    }
     
 
     /**
