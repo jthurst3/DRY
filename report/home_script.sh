07f6267af5aa7fa24da98125536c5226b09ade0c
J. Hassler Thurston <jthurst3@u.rochester.edu>
2014-12-03 23:52:40 -0500

    modify report and home script

--- report/home_script.sh
+++ report/home_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ssh jthurst3@cycle1.csug.rochester.edu 'cd Courses/CSC200/honors/report; ./compiletex.sh'
-scp jthurst3@cycle1.csug.rochester.edu:~/Courses/CSC200/cs200repo/honors/report/report.pdf .
-open assign8.pdf
+ssh jthurst3@cycle1.csug.rochester.edu 'cd Courses/CSC200/honors/report; rake'
+scp jthurst3@cycle1.csug.rochester.edu:~/Courses/CSC200/honors/report/report.pdf .
+open report.pdf
